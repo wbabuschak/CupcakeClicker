9343b07121f4db6ba47017798d848b0685347ffd
wbabuschak <wcb40716@gmail.com>
2025-01-06 10:30:57 -0500

    update 0.0.6
    -added autoclickers
    -reworked background logic (again)

--- game/src/main/java/wbabuschak/GUI.java
+++ game/src/main/java/wbabuschak/GUI.java
@@ -6,11 +6,16 @@ import java.text.*;
 
 public class GUI {
 
-    String title = "Cupcake Clicker v0.0.5";
+    String title = "Cupcake Clicker v0.0.6";
 
     private static int CUPCAKE_GOAL = 1000000;
     private static int SUPERBAKE_COOLDOWN = 50;
-    private static int AUTO_SUPER_COST = 50000;
+    private static int AUTO_SUPER_COST = 7500;
+    private static int CLICKER_SUPER_COST = 15000;
+    private static int TICK_RATE = 100;
+
+    private Timer gameClock;
+    private int tick = 0;
 
     private int superbakeEffects = 10;
 
@@ -22,13 +27,16 @@ public class GUI {
     private Boolean superbake = false;
 
     private Boolean autoSuper = false;
+    private Boolean clickerSuper = false;
     
     private int upgrades = 0;
     private int doublers = 0;
+    private int clickers = 0;
 
     private int upgradePrice = 10 * (upgrades + 1) * (bakeCount + upgrades);
     private int doublerPrice = (int) Math.pow(10, doublers + 1);
     private int upgradeSuperbakePrice = (int) Math.pow(2, superbakeEffects);
+    private int autoclickerPrice = 100 * (int) Math.pow(12, clickers + 1);
 
     private JLabel cupcakeLabel;
     private JLabel actionLabel;
@@ -43,6 +51,8 @@ public class GUI {
     private JButton superbakeUpgradeButton;
     private JButton autoSuperButton;
     private JButton winButton;
+    private JButton autoclickerButton;
+    private JButton autoclickerSuperbakeButton;
 
     private JFrame backFrame;
     private JPanel mainPanel;
@@ -52,6 +62,8 @@ public class GUI {
     private static Color PURCHASED_COLOR = new Color(176,141,87);
 
     public GUI() {
+        gameClock = new Timer(TICK_RATE, e -> clockUpdate());
+
         backFrame = new JFrame();
         mainPanel = new JPanel();
         updateBackground();
@@ -116,6 +128,18 @@ public class GUI {
         setSize(autoSuperButton, 1);
         updateAutoSuperButton();
 
+        // autoclickerButton
+        autoclickerButton = new JButton();
+        autoclickerButton.addActionListener( e -> buyAutoClicker());
+        setSize(autoclickerButton, 0);
+        updateAutoClickerButton();
+
+        // autoclickerSuperbakeButton
+        autoclickerSuperbakeButton = new JButton();
+        autoclickerSuperbakeButton.addActionListener( e -> buyAutoclickerSuperbake());
+        setSize(autoclickerSuperbakeButton, 1);
+        updateAutoclickerSuperbakeButton();
+
         // winButton
         winButton = new JButton();
         winButton.addActionListener( e -> checkWin());
@@ -135,6 +159,9 @@ public class GUI {
 
         addElement(upgradeButton, 1, 3);
         addElement(doublerButton, 2, 3);
+
+        addElement(autoclickerButton, 3, 0);
+        addElement(autoclickerSuperbakeButton, 3, 2);
         
 
         // backFrame
@@ -147,12 +174,16 @@ public class GUI {
         Image image = icon.getImage();
         backFrame.setIconImage(image);
         backFrame.setVisible(true);
+
+        gameClock.start();
     }
 
     private void update() {
         upgradePrice = 10 * (upgrades + 1) * (bakeCount + upgrades);
         doublerPrice = (int) Math.pow(10, doublers + 1);
         upgradeSuperbakePrice = (int) Math.pow(2, superbakeEffects);
+        autoclickerPrice = 100 * (int) Math.pow(12, clickers + 1);
+
        
         updateBakeButton();
         updateUpgradeButton();
@@ -161,10 +192,42 @@ public class GUI {
         updateUpgradeSuperbakeButton();
         updateAutoSuperButton();
         updateWinButton();
+        updateAutoClickerButton();
+        updateAutoclickerSuperbakeButton();
 
         updateCupcakeLabel();
         updateProgress();
         updateBackground();
+    }
+
+    private void clockUpdate() {
+        tick++;
+        // autoclickers
+        if (tick % 10 == 0){
+            for (int i = 0; i < clickers; i++){
+                if (clickerSuper){
+                    if (superbake){
+                        cupcakes += 2 * bakeCount * (int) Math.pow(2, doublers);
+                        superbakecnt++;
+                    } else {
+                        cupcakes += bakeCount * (int) Math.pow(2, doublers);
+                        superbakecd++;
+                    }
+                    if (superbakecnt >= superbakeEffects){
+                        superbake = false;
+                        superbakecnt = 0;
+                        superbakecd = 0;
+                    }
+                    
+                    if (autoSuper){
+                        superbake();
+                    }
+                } else {
+                    cupcakes += bakeCount * (int) Math.pow(2, doublers);
+                }
+            }
+        }
+        update();
     }
 
     private void updateBakeButton(){
@@ -181,7 +244,7 @@ public class GUI {
         activateButton(cupcakes >= upgradePrice,upgradeButton);
     }
     private void updateDoublerButton(){
-        doublerButton.setToolTipText("Price: " + doublerPrice + " = 10 ^ (doublers {" + doublers + "} + 1)");
+        doublerButton.setToolTipText("Price: " + doublerPrice + " = 100 ^ (doublers {" + doublers + "} + 1)");
         doublerButton.setText("Buy Doubler! (" + doublerPrice + ")");
         activateButton(cupcakes >= doublerPrice,doublerButton);
     }
@@ -213,12 +276,26 @@ public class GUI {
             autoSuperButton.setText("Puchase Automatic Superbaker: (" + AUTO_SUPER_COST + ")");
         }
     }
+    private void updateAutoclickerSuperbakeButton(){
+        autoclickerSuperbakeButton.setToolTipText("Allows your clickers to utilize Superbake");
+        activateButton(cupcakes >= CLICKER_SUPER_COST && clickerSuper == false, autoclickerSuperbakeButton);
+        if (clickerSuper){
+            autoclickerSuperbakeButton.setText("Clickers Superbaking!");
+            autoclickerSuperbakeButton.setBackground(PURCHASED_COLOR);
+        } else {
+            autoclickerSuperbakeButton.setText("Puchase Clicker Superbaking: (" + CLICKER_SUPER_COST + ")");
+        }
+    }
     private void updateWinButton(){
         winButton.setToolTipText("Goal: " + commaFormat.format(CUPCAKE_GOAL));
         winButton.setText("Win! (closes game)");
         activateButton(cupcakes >= CUPCAKE_GOAL, winButton);
     }
-
+    private void updateAutoClickerButton(){
+        autoclickerButton.setToolTipText("Price: " + autoclickerPrice + " = 100 * 12 ^ (autoclickers {" + clickers + "} + 1)");
+        autoclickerButton.setText("Buy Autoclicker! (" + autoclickerPrice + ")");
+        activateButton(cupcakes >= autoclickerPrice,autoclickerButton);
+    }
     private void updateProgress(){
         progressBar.setToolTipText("Cupcakes Baked: " + progressBar.getValue() + " / " + commaFormat.format(CUPCAKE_GOAL));
         progressBar.setValue(cupcakes);
@@ -227,7 +304,7 @@ public class GUI {
         cupcakeLabel.setText("Cupcakes: " + commaFormat.format(cupcakes));
     }
     private void updateBackground(){
-        mainPanel.setBackground(new Color(255, 130 + (int) (15 * (Math.min(7, Math.log10(CUPCAKE_GOAL / (cupcakes + 1))))), 130 + (int) (15 * (Math.min(7, Math.log10(CUPCAKE_GOAL / (cupcakes + 1)))))));
+        mainPanel.setBackground(new Color(255, 225 - (int) (95 * Math.min(1,(Math.log10(cupcakes + 1)/Math.log10(CUPCAKE_GOAL)))), 225 - (int) (95 * Math.min(1,(Math.log10(cupcakes + 1)/Math.log10(CUPCAKE_GOAL))))));
     }
 
     private void checkWin(){
@@ -321,11 +398,31 @@ public class GUI {
             autoSuper = true;
             actionLabel.setText("Upgrade purchased! -" + AUTO_SUPER_COST + " cupcakes!");
         } else {
-            actionLabel.setText("Insufficient funds to purchase Auto Superbaker.");
+            actionLabel.setText("Insufficient funds to purchase Automatic Superbaker.");
         }
         update();
     }
-    
+    private void buyAutoclickerSuperbake() {
+        if (cupcakes >= CLICKER_SUPER_COST){
+            cupcakes -= CLICKER_SUPER_COST;
+            clickerSuper = true;
+            actionLabel.setText("Upgrade purchased! -" + CLICKER_SUPER_COST + " cupcakes!");
+        } else {
+            actionLabel.setText("Insufficient funds to purchase Autoclicker Superbaking.");
+        }
+        update();
+    }
+    private void buyAutoClicker() {
+        if (cupcakes >= autoclickerPrice){
+            cupcakes -= autoclickerPrice;
+            clickers++;
+            actionLabel.setText("Autoclicker purchased! -" + autoclickerPrice + " cupcakes!");
+        } else {
+            actionLabel.setText("Insufficient funds to purchase Autoclicker.");
+        }
+        update();
+    }
+
     private void addElement(JComponent component, int x, int y){
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = x;
